9186e34ce6f908327d39ee555a002e78ed373bcf
Norfol <frolmt@gmail.com>
2025-03-23 18:03:58 +0300

    Update Epic.java

--- src/main/java/ru/netologia/qamid/services/Epic.java
+++ src/main/java/ru/netologia/qamid/services/Epic.java
@@ -22,7 +22,7 @@ public class Epic extends Task {
 
         // Проверяем каждую подзадачу на наличие query
         for (String subtask : subtasks) {
-            if (subtask != subtask.contains(query)) {
+            if (subtask.contains(query)) {
                 return true; // Возвращаем true, если найдено совпадение
             }
         }
